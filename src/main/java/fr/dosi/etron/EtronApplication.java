2f222f314639c44823034a6f3cd7da0856b9074b
ayoubmazouni <ayoubmazouni@gmail.com>
2022-01-24 10:43:56 +0100

    initial commit

---
+++ src/main/java/fr/dosi/etron/EtronApplication.java
@@ -0,0 +1,13 @@
+package fr.dosi.etron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtronApplication.class, args);
+    }
+
+}
